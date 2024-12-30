1b804cd701176497870896e78d7b958d41f46b77
Henry Adagala <adagalahenry@gmail.com>
2024-12-30 14:24:06 +0300

    update: gallery images

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -3,19 +3,46 @@ import Footer from "@/components/Footer";
 import Image from "next/image";
 
 const images = [
-  { id: 1, src: "/placeholder.svg?height=300&width=400", alt: "Paris" },
-  { id: 2, src: "/placeholder.svg?height=300&width=400", alt: "Tokyo" },
-  { id: 3, src: "/placeholder.svg?height=300&width=400", alt: "Bali" },
-  { id: 4, src: "/placeholder.svg?height=300&width=400", alt: "New York" },
-  { id: 5, src: "/placeholder.svg?height=300&width=400", alt: "Safari" },
-  { id: 6, src: "/placeholder.svg?height=300&width=400", alt: "Greek Islands" },
-  { id: 7, src: "/placeholder.svg?height=300&width=400", alt: "Machu Picchu" },
+  {
+    id: 1,
+    src: "/amboseli-national-park.jpg?height=300&width=400",
+    alt: "Amboseli National Park",
+  },
+  {
+    id: 2,
+    src: "/masaimara-main.jpg?height=300&width=400",
+    alt: "Maasai Mara",
+  },
+  {
+    id: 3,
+    src: "/zanzibar-island.jpg?height=300&width=400",
+    alt: "Zanzibar Island",
+  },
+  {
+    id: 4,
+    src: "/taita-hills-safari-resort-spa.jpg?height=300&width=400",
+    alt: "Taita Hills Safari Resort Spa",
+  },
+  {
+    id: 5,
+    src: "/neptunevillage.jpg?height=300&width=400",
+    alt: "Neptune Village",
+  },
+  {
+    id: 6,
+    src: "/salt-lick-safari-lodge-train.jpg?height=300&width=400",
+    alt: "Salt Lick Safari Lodge",
+  },
+  {
+    id: 7,
+    src: "/naivasha-hotels.jpg?height=300&width=400",
+    alt: "Naivasha Hotels",
+  },
   {
     id: 8,
-    src: "/placeholder.svg?height=300&width=400",
-    alt: "Great Barrier Reef",
+    src: "/mombasa-hotels.jpg?height=300&width=400",
+    alt: "Mombasa Hotels",
   },
-  { id: 9, src: "/placeholder.svg?height=300&width=400", alt: "Venice" },
 ];
 
 export default function GalleryPage() {
